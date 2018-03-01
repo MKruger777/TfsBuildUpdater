d82eb77805062e2eca32097975ffdda17cf25735
Unknown <morne.kruger@gmail.com>
2018-03-01 15:44:14 +0100

    Some more steps to get into PSObj

--- Get-TfsBldDef.ps1
+++ Get-TfsBldDef.ps1
@@ -26,17 +26,21 @@ function Get-TfsBldDef
     #$wiqlUrl = "$TfsUri/$TfsCollection/_apis/projects?api-version=1.0"
     #$wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions?api-version=2.0"
     $wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions/1?api-version=2.0"  # WOOOORKS!!!!!
-    #https://fabrikam-fiber-inc.visualstudio.com/DefaultCollection/Fabrikam-Fiber-Git/_apis/build/definitions/3?api-version=1.0
-
-    #$secpasswd = ConvertTo-SecureString "H@nspeterHeidi2" -AsPlainText -Force
-    #$mycreds = New-Object System.Management.Automation.PSCredential ("morne.kruger@outlook.com", $secpasswd)
-
 
     $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "morne.kruger@outlook.com","2hrvgyuonfsffwpq4anfcsvzqp2wxpxyjurvvxtgj34y26dpukda")))
     $JsonResult = Invoke-RestMethod -uri $wiqlUrl -Method Get -ContentType 'application/Json' -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
-    Write-Host $definitionsOverviewResponse.Content
+    #$definitionsOverview = ConvertFrom-Json $JsonResult  #original
+    #test
+    #https://github.com/MultiPoolMiner/MultiPoolMiner/issues/420
+    $JsonParameters = ConvertFrom-Json -InputObject $JsonResult
+    #
+    
+    $definitionUrl = ($definitionsOverview | Where-Object { $_.name -eq $targetBuildName } | Select-Object -First 1).url
+    $response = Invoke-WebRequest $buildDefinitionUrl -UseDefaultCredentials
 
+
+    Write-Host $definitionsOverviewResponse.Content
 
     Write-Host "Tfs projects found for collection $TfsCollection = " $JsonResult.Count
     if($JsonResult.Count -gt 0)
