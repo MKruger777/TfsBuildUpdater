e4debb8bf314c8d6033fb199f6f4f9dd53dd53c6
Unknown <morne.kruger@gmail.com>
2018-03-01 23:35:59 +0100

    Detection VSTest and Npm works

--- Get-TfsBldDef.ps1
+++ Get-TfsBldDef.ps1
@@ -3,8 +3,18 @@ function Get-TfsBldDef
 {
         <#
         .SYNOPSIS
-        As part of certain release activities, it is nessasary to retrieve a set of Tfs work items.
-        This function will retrieve the set on basis of the WIQL query passed to it.
+        
+        It seems possible to update the PSCustom object that was returned  
+        The plan is thus:
+        1) Get the build def
+        2) Scan it to see if there is somthing that needs updating
+        3) Updat the build definition as required witin the PSObject - plain .(dot) notation
+        this seems to work fine
+        4) Then convert to JSon 
+        5) Do the PUT passing the JSon as body 
+        6) Check the return code
+        7) Log our action for audit purposes
+        
 
         .PARAMETER WiqlQuery
         This is the query that will determine the workitems selected and returned to the caller.
@@ -25,7 +35,7 @@ function Get-TfsBldDef
     $TfsProjects = New-Object System.Collections.Generic.List[System.Object]
     
     #$wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions/1?api-version=2.0"  #WORKS! - VSTS
-    $wiqlUrl = "http://t800:8080/tfs/DefaultCollection/Discovery/_apis/build/definitions/1?api-version=2.0"   #WORKS! - T800
+    $wiqlUrl = "http://t800:8080/tfs/DefaultCollection/Discovery/_apis/build/definitions/2?api-version=2.0"   #WORKS! - T800
 
     $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "T800\morne","en55denwlgpdxw2t4bwkdq6apfbugspjaxbhjhrxvymex5tqb2aa")))
 
@@ -40,6 +50,7 @@ function Get-TfsBldDef
     #Search for your stuff and update if required
     foreach($BldTask in $JsonResult.Build)
     {
+        Write-Host $BldTask.DisplayName.ToLower() 
         if($BldTask.DisplayName.ToLower().Contains("nuget"))
         {
             Write-Host "Build display name: " $BldTask.DisplayName
@@ -48,43 +59,53 @@ function Get-TfsBldDef
             {
                 if($Prop.name.ToLower().Contains("nugetversion"))
                 {
+                    Write-Host "NuGet version found!" -ForegroundColor Yellow
                     Write-Host "Property = " $Prop.Name
                     Write-Host "Value = " $Prop.Value
                 }                
             }
-        }        
+        }
+        
+        #now check the Vstest versions stuff
+        if($BldTask.DisplayName.ToLower().Contains("vstest - testassemblies"))
+        {
+            if($BldTask.Task.VersionSpec -eq "2.*")
+            {
+                Write-Host "VsTest versionspec 2 detected! " -ForegroundColor Yellow      
+            }
+        }
+
+        #now check the Vstest versions stuff
+        if($BldTask.DisplayName.ToLower().Contains("npm"))
+        {
+            foreach($Prop in $BldTask.inputs.PSObject.Properties)
+            {
+                if($Prop.name.ToLower().Contains("command"))
+                {
+                    Write-Host "Npm command found!" -ForegroundColor Yellow
+                    Write-Host "Property = " $Prop.Name
+                    Write-Host "Value = " $Prop.Value
+                }                
+            }
+        }
     }
 
 
+    #temp test
+    $JsonResult.name += "Hans-Peter"
 
     #now we need to convert to JSon to update some values? ...
     $JsonParameters = ConvertTo-Json -InputObject $JsonResult
     
-    <#
-
-    It seems possible to update the PSCustom object that was returned  
-    The plan is thus:
-    1) Get the build def
-    2) Scan it to see if there is somthing that needs updating
-    3) Updat the build definition as required witin the PSObject - plain .(dot) notation
-       this seems to work fine
-    4) Then convert to JSon 
-    5) Do the PUT passing the JSon as body 
-    6) Check the return code
-    7) Log our action for audit purposes
-
-    #>
+    # Now lets send the sucker back and update the definition
+    #example
+    #https://fabrikam-fiber-inc.visualstudio.com/DefaultCollection/Fabrikam-Fiber-Git/_apis/build/definitions/29?api-version=2.0
+   
+    $JsonResult = Invoke-RestMethod -uri $wiqlUrl -Method Put -Body $JsonParameters -ContentType 'application/Json' -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    
 
 
 
-
-
-    #update pieces
-    $definitionUrl = ($definitionsOverview | Where-Object { $_.name -eq $targetBuildName } | Select-Object -First 1).url
-    $response = Invoke-WebRequest $buildDefinitionUrl -UseDefaultCredentials
-    #
-
-    Write-Host $definitionsOverviewResponse.Content
 
     Write-Host "Tfs projects found for collection $TfsCollection = " $JsonResult.Count
     if($JsonResult.Count -gt 0)
