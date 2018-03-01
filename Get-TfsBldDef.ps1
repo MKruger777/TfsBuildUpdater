06cc2e7784bd2695d13ad585ea0565d814104576
Unknown <morne.kruger@gmail.com>
2018-03-01 18:09:45 +0100

    Auto stash before merge of "master" and "origin/master"
    Can now sniff out GitVersion in BuildTask!

--- Get-TfsBldDef.ps1
+++ Get-TfsBldDef.ps1
@@ -23,22 +23,66 @@ function Get-TfsBldDef
     Write-Host "TfsCollection : $TfsCollection"
 
     $TfsProjects = New-Object System.Collections.Generic.List[System.Object]
-    #$wiqlUrl = "$TfsUri/$TfsCollection/_apis/projects?api-version=1.0"
-    #$wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions?api-version=2.0"
-    $wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions/1?api-version=2.0"  # WOOOORKS!!!!!
-
-    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "morne.kruger@outlook.com","2hrvgyuonfsffwpq4anfcsvzqp2wxpxyjurvvxtgj34y26dpukda")))
-    $JsonResult = Invoke-RestMethod -uri $wiqlUrl -Method Get -ContentType 'application/Json' -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
-
-    #$definitionsOverview = ConvertFrom-Json $JsonResult  #original
-    #test
-    #https://github.com/MultiPoolMiner/MultiPoolMiner/issues/420
-    $JsonParameters = ConvertFrom-Json -InputObject $JsonResult
-    #
     
+    #$wiqlUrl = "https://krugers.visualstudio.com/Build-Discovery/_apis/build/definitions/1?api-version=2.0"  #WORKS! - VSTS
+    $wiqlUrl = "http://t800:8080/tfs/DefaultCollection/Discovery/_apis/build/definitions/1?api-version=2.0"   #WORKS! - T800
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "T800\morne","en55denwlgpdxw2t4bwkdq6apfbugspjaxbhjhrxvymex5tqb2aa")))
+
+    #This call returns a powershell custom object...
+    $JsonResult = Invoke-RestMethod -uri $wiqlUrl -Method Get -ContentType 'application/Json' -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    Write-Host "Build name: " $JsonResult.name
+    Write-Host "Build ID: " $JsonResult.ID
+    Write-Host "Build Type: " $JsonResult.type
+    
+    #TODO - Ignore XAML builds and log it!
+    
+    #Search for your stuff and update if required
+    foreach($BldTask in $JsonResult.Build)
+    {
+        if($BldTask.DisplayName.ToLower().Contains("nuget"))
+        {
+            Write-Host "Build display name: " $BldTask.DisplayName
+            #Write-Host "Configuration properties are:"
+            foreach($Prop in $BldTask.inputs.PSObject.Properties)
+            {
+                if($Prop.name.ToLower().Contains("nugetversion"))
+                {
+                    Write-Host "Property = " $Prop.Name
+                    Write-Host "Value = " $Prop.Value
+                }                
+            }
+        }        
+    }
+
+
+
+    #now we need to convert to JSon to update some values? ...
+    $JsonParameters = ConvertTo-Json -InputObject $JsonResult
+    
+    <#
+
+    It seems possible to update the PSCustom object that was returned  
+    The plan is thus:
+    1) Get the build def
+    2) Scan it to see if there is somthing that needs updating
+    3) Updat the build definition as required witin the PSObject - plain .(dot) notation
+       this seems to work fine
+    4) Then convert to JSon 
+    5) Do the PUT passing the JSon as body 
+    6) Check the return code
+    7) Log our action for audit purposes
+
+    #>
+
+
+
+
+
+    #update pieces
     $definitionUrl = ($definitionsOverview | Where-Object { $_.name -eq $targetBuildName } | Select-Object -First 1).url
     $response = Invoke-WebRequest $buildDefinitionUrl -UseDefaultCredentials
-
+    #
 
     Write-Host $definitionsOverviewResponse.Content
 
