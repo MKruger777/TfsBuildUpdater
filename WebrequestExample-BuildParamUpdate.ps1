d1db5b6f8e45648d45fa13645dcd5c411d1f3628
Unknown <morne.kruger@gmail.com>
2018-03-06 21:33:20 +0100

    works to update build def with loosing tasks

---
+++ WebrequestExample-BuildParamUpdate.ps1
@@ -0,0 +1,22 @@
+﻿
+
+#this code works to update the VNext build definition without chucking the tasks away
+
+$BldDefinitionUrl = "http://t800:8080/tfs/DefaultCollection/Discovery/_apis/build/definitions/3?api-version=2.0"
+$response = Invoke-WebRequest $BldDefinitionUrl -UseDefaultCredentials
+
+# This assumes the working directory is the location of the assembly:
+[void][System.Reflection.Assembly]::LoadFile("C:\dev\PowerShell\Tfs-BuildDefinitions\TfsBuildUpdater\newtonsoft.json.11.0.1\lib\net45\Newtonsoft.Json.dll")
+$buildDefinition = [Newtonsoft.Json.JsonConvert]::DeserializeObject($response.Content)
+Write-Host "BuildDef name=" $buildDefinition.name.ToString()
+
+$buildDefinition.name = "Captain America"
+
+$serialized = [Newtonsoft.Json.JsonConvert]::SerializeObject($buildDefinition)
+
+$postData = [System.Text.Encoding]::UTF8.GetBytes($serialized)
+# The TFS2015 REST endpoint requires an api-version header, otherwise it refuses to work properly.
+$headers = @{ "Accept" = "api-version=2.3-preview.2" }
+$response = Invoke-WebRequest -UseDefaultCredentials -Uri $BldDefinitionUrl -Headers $headers `
+              -Method Put -Body $postData -ContentType "application/json"
+$response.StatusDescription
