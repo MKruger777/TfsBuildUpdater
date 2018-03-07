149bb8bfbd0e55a1eb17fc082a3d5c409dde4394
Unknown <mkruger@binck.nl>
2018-03-07 11:33:36 +0100

    Auto stash before merge of "master" and "origin/master"

--- WebrequestExample.ps1
+++ WebrequestExample.ps1
@@ -4,29 +4,28 @@ $targetCollection = "DefaultCollection"
 $targetProject = "Acme"
 $targetBuildName = "Acme - My Build Definition"
 #$definitionsOverviewUrl = "$baseUrl/$targetCollection/$targetProject/_apis/build/Definitions"
-#lets assume that the build def will be passed in ...
- 
-$definitionsOverviewUrl = "http://t800:8080/tfs/DefaultCollection/Discovery/_apis/build/Definitions" 
-$definitionsOverviewResponse = Invoke-WebRequest -UseDefaultCredentials -Uri $definitionsOverviewUrl
+$definitionsOverviewUrl = "http://papptfs17.binckbank.nv:8080/tfs/Binck/Delivery/_apis/build/Definitions" 
+$definitionsOverviewResponse = Invoke-WebRequest -UseDefaultCredentials $definitionsOverviewUrl
+$NewtonSoftJsonPath = "D:\Dev\github\TfsBuildUpdater\newtonsoft.json.11.0.1\lib\net45\Newtonsoft.Json.dll"
 
 Write-Host "content=" $definitionsOverviewResponse.Content
 
 $definitionsOverview = (ConvertFrom-Json $definitionsOverviewResponse.Content).value
-$BldDefinitionUrl = ($definitionsOverview | Where-Object { $_.name -eq "SuperHero" } | Select-Object -First 1).url
+$BldDefinitionUrl = ($definitionsOverview | Where-Object { $_.name -eq "We build this city" } | Select-Object -First 1).url
 $response = Invoke-WebRequest $BldDefinitionUrl -UseDefaultCredentials
 #as the variable states we get the build def URL ...
 
 Write-Host "BuildDef content =" $response.Content
 
 # This assumes the working directory is the location of the assembly:
-[void][System.Reflection.Assembly]::LoadFile("C:\dev\PowerShell\Tfs-BuildDefinitions\TfsBuildUpdater\newtonsoft.json.11.0.1\lib\net45\Newtonsoft.Json.dll")
+[void][System.Reflection.Assembly]::LoadFile($NewtonSoftJsonPath)
 $buildDefinition = [Newtonsoft.Json.JsonConvert]::DeserializeObject($response.Content)
 Write-Host "BuildDef name=" $buildDefinition.name.ToString()
 
 # JObjects implement IDictionary and therefore support dot notation
 #$buildDefinition.variables.MajorMinor.value.ToString()
 #$buildDefinition.variables.MajorMinor.value = "3.4"
-$buildDefinition.name = "Batman magic trick"
+$buildDefinition.name = "Captain America"
 
 $serialized = [Newtonsoft.Json.JsonConvert]::SerializeObject($buildDefinition)
 
